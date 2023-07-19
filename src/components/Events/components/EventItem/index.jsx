394b749019af53cce1126bc00344f645000c792d
Juan Ortiz Jr <juanjo.jjo94@gmail.com>
2023-07-19 15:00:32 -0600

    Add like/unlike functionality

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,15 +1,27 @@
 import styles from './EventItem.module.css';
 
+import useLikeEvents from '../../../../hooks/useLikeEvents';
+import HearthFilled from '../../../../assets/hearth-filled.png';
+import HearthUnfilled from '../../../../assets/hearth-unfilled.png';
+
 const EventItem = ({ info, id, name, image, onEventClick }) => {
+    const { isEventLiked, toggleEventLike } = useLikeEvents(id);
     
     const handleSeeMoreClick = (evt) => {
         evt.stopPropagation();
         onEventClick(id);
     };
+
+    const handleHearthClick = () => {
+        toggleEventLike();
+    };
     
     return (
         <div onClick={() => console.log('padre clickeado')} className={`${styles.eventItemContainer} ${styles.anotherContainer}`}>
-            <img src={image} alt={name} width={200} height={200} />
+            <div className={styles.imageContainer}>
+                <img src={isEventLiked ? HearthFilled : HearthUnfilled} alt="Hearth button" className={styles.hearthImage} onClick={handleHearthClick} />
+                <img src={image} alt={name} width={200} height={200} />
+            </div>
             <div className={styles.eventInfoContainer}>
                 <h4 className={styles.eventName}>{name}</h4>
                 <p className={styles.eventInfo}>{info}</p>
---
+++ src/hooks/useLikeEvents.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const LIKED_EVENTS_STORAGE_KEY = 'likedEvents';
+
+const checkIsEventLiked = (eventId) => {
+    const likedEvents = JSON.parse(localStorage.getItem(LIKED_EVENTS_STORAGE_KEY)) || [];
+
+    console.log(likedEvents)
+    return likedEvents.includes(eventId);
+};
+
+const useLikeEvents = (eventId) => {
+    const [isEventLiked, setIsEventLiked] = useState(checkIsEventLiked(eventId));
+
+    const toggleEventLike = () => {
+        const likedEvents = JSON.parse(localStorage.getItem(LIKED_EVENTS_STORAGE_KEY)) || [];
+        const eventIndex = likedEvents.indexOf(eventId);
+
+        if (eventIndex !== -1) {
+            likedEvents.splice(eventIndex, 1);
+            setIsEventLiked(false);
+        } else {
+            likedEvents.push(eventId);
+            setIsEventLiked(true);
+        }
+
+        localStorage.setItem(LIKED_EVENTS_STORAGE_KEY, JSON.stringify(likedEvents));
+    };
+
+    return {
+        isEventLiked,
+        toggleEventLike,
+    };
+};
+
+export default useLikeEvents;
